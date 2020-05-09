4255845e5ac49180c8b7a827127560d836dade74
polojerry <polojerry4@gmail.com>
2020-05-09 16:27:14 +0300

    *Used when expression

--- src/l2/AMS.kt
+++ src/l2/AMS.kt
@@ -2,14 +2,14 @@ package l2
 
 import kotlin.random.Random
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
     println("Hello ${args[0]}")
     feedTheFish()
 }
 
 fun feedTheFish() {
     val day = randomDay()
-    val food = "pellets"
+    val food = fishFood(day)
 
     println("Today is ${day}, the fish eats $food")
 }
@@ -18,4 +18,19 @@ fun randomDay(): String {
     val week = listOf("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
     return week[Random.nextInt(7)]
 
+}
+
+fun fishFood(day: String): String {
+    return when (day) {
+        "Sunday" -> "plankton"
+        "Monday" ->"flakes"
+        "Tuesday" -> "pellets"
+        "Wednesday" -> "red worms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Saturday" -> "lettuces"
+        else -> "fasting"
+
+
+    }
 }
